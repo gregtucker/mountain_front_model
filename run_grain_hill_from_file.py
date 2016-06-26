549ea4f518888c2bf092335c4feba7c7fe548ea8
Greg Tucker <gtucker@colorado.edu>
2016-06-26 09:44:38 -0600

    script and file to run from file

---
+++ run_grain_hill_from_file.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 26 09:13:46 2016
+
+@author: gtucker
+"""
+
+import grain_hill_as_class
+
+params = grain_hill_as_class.get_params_from_input_file('grain_hill_sample_inputs.txt')
+grain_hill_as_class.main(params)
