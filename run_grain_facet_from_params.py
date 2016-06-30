73dca537793f7bb6bfe5cacfc9074b8d25ce932b
Greg Tucker <gtucker@colorado.edu>
2016-06-30 15:30:32 -0600

    add run from params

---
+++ run_grain_facet_from_params.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+A note on time scales, in seconds:
+
+Duration
+(sec)  (equiv)
+--------------
+1 s    ~     1 s
+10 s   ~     1 min
+100 s  ~     1 min
+1000 s ~     1 hr
+10,000 s   ~  1 hr
+10^5 s     ~  1 day (28 hrs)
+10^6 s     ~  1 week (12 days)
+10^7 s     ~  3 months
+10^8 s     ~  3 years
+
+Created on Sun Jun 26 09:13:46 2016
+
+@author: gtucker
+"""
+
+import grain_facet_model as gfm
+
+params = {
+    'number_of_node_rows' : 40,
+    'number_of_node_columns' : 61,
+    'report_interval' : 5.0,
+    'run_duration' : 1.0e6,
+    'plot_interval' : 5.0e4,
+    'uplift_interval' : 5.0e4,
+    'disturbance_rate' : 1.0e-4,
+    'weathering_rate' : 1.0e-5,
+    'friction_coef' : 1.0,
+    'fault_x' : 8.0
+    }
+gfm.main(params)
