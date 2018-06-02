915c601c5395418414f5534437d4ddb635fc9fca
Greg Tucker <gtucker@colorado.edu>
2018-06-02 11:36:39 -0600

    update run_grain_facet_from_params.py

--- run_grain_facet_from_params.py
+++ run_grain_facet_from_params.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-
+run_grain_facet_from_params.py: demonstrates how to instantiate and run
+a GrainFacetSimulator from another Python script, passing parameters via
+a dictionary rather than using a separate input file.
 
 A note on time scales, in seconds:
 
@@ -21,23 +23,29 @@ Created on Sun Jun 26 09:13:46 2016
 
 @author: gtucker
 """
-import time
 
-import grain_facet_model as gfm
+import time
+from grain_facet_model import GrainFacetSimulator
 
 params = {
-    'number_of_node_rows' : 40,
-    'number_of_node_columns' : 61,
+    'number_of_node_rows' : 20,
+    'number_of_node_columns' : 31,
     'report_interval' : 5.0,
-    'run_duration' : 5.0e5,
-    'plot_interval' : 5.0e4,
-    'uplift_interval' : 5.0e3,
-    'disturbance_rate' : 1.0e-4,
-    'weathering_rate' : 1.0e-5,
+    'run_duration' : 150.0,
+    'output_interval' : 1000.0,
+    'plot_interval' : 10.0,
+    'uplift_interval' : 10.0,
+    'disturbance_rate' : 0.01,
+    'weathering_rate' : 0.002,
     'friction_coef' : 1.0,
-    'fault_x' : 8.0
+    'fault_x' : 8.0,
+    'cell_width' : 1.0
     }
+
 start = time.time()
-gfm.main(params)
-print 'Run time:', time.time() - start
+gridsize = (params['number_of_node_rows'], params['number_of_node_columns'])
+gfs = GrainFacetSimulator(gridsize, **params)
+gfs.run()
+print('Run complete. Run time (sec):')
+print(time.time() - start)
 
